6b3aaabb5f11aba87b437a87606076f1046f4bd8
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-31 17:02:21 +0200

    Add files via upload

---
+++ ADO.NET/Increase Age Stored Procedure/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Increase_Age_Stored_Procedure
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int id = int.Parse(Console.ReadLine());
+
+            SqlConnection connection = new SqlConnection("Server=DESKTOP-HUSD35E\\SQLEXPRESS;" + "Database=MinionsDB;" + "Integrated Security = true;");
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand increaseAge = new SqlCommand("EXEC usp_GetOlder @Id", connection);
+                increaseAge.Parameters.AddWithValue("@Id", id);
+
+                SqlDataReader increaseReader = increaseAge.ExecuteReader();
+
+                increaseReader.Close();
+
+                SqlCommand printMinion = new SqlCommand("SELECT Name, Age FROM Minions WHERE Id = @Id", connection);
+                printMinion.Parameters.AddWithValue("@Id", id);
+
+                SqlDataReader print = printMinion.ExecuteReader();
+                print.Read();
+
+                Console.WriteLine($"{print["Name"]} – {print["Age"]} years old");
+
+                print.Close();
+            }
+        }
+    }
+}
