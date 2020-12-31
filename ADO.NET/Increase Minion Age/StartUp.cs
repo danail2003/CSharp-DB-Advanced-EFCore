4ff430649a4b2b1f3b998124a8d4bf56cc6c730d
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-31 17:02:59 +0200

    Add files via upload

---
+++ ADO.NET/Increase Minion Age/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace Increase_Minion_Age
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] ids = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SqlConnection connection = new SqlConnection("Server=DESKTOP-HUSD35E\\SQLEXPRESS;" + "Database=MinionsDB;" + "Integrated Security = true;");
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand increaseAge = new SqlCommand(@"UPDATE Minions
+                                                        SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                                        WHERE Id = @Id", connection);
+
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    increaseAge.Parameters.AddWithValue("@Id", ids[i]);
+
+                    SqlDataReader increaseReader = increaseAge.ExecuteReader();
+
+                    increaseReader.Close();
+                    increaseAge.Parameters.Clear();
+                }
+
+                SqlCommand printNamesCommand = new SqlCommand("SELECT Name, Age FROM Minions", connection);
+
+                SqlDataReader print = printNamesCommand.ExecuteReader();
+
+                while (print.Read())
+                {
+                    Console.WriteLine($"{print["Name"]} {print["Age"]}");
+                }
+            }
+        }
+    }
+}
