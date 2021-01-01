afc618358e406bbbf80e0a75ceab04f36f1b5218
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-01 10:08:25 +0200

    Add files via upload

---
+++ ADO.NET/Villain Names/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Villain_Names
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            SqlConnection connection = new SqlConnection("Server=DESKTOP-HUSD35E\\SQLEXPRESS;" + "Database=MinionsDB;" + "Integrated Security=true;");
+
+            string query= @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                            FROM Villains AS v
+                            JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                            GROUP BY v.Id, v.Name
+                            HAVING COUNT(mv.VillainId) > 3
+                            ORDER BY COUNT(mv.VillainId)";
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                }
+            }
+        }
+    }
+}
