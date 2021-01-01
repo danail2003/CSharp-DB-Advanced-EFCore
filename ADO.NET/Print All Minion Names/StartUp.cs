1ba6b06b6a0afc2e178d1f62f4af465b7197e4e2
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-01 10:07:39 +0200

    Add files via upload

---
+++ ADO.NET/Print All Minion Names/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace Print_All_Minion_Names
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            List<string> names = new List<string>();
+            string query = "SELECT Name FROM Minions";
+
+            SqlConnection connection = new SqlConnection("Server=DESKTOP-HUSD35E\\SQLEXPRESS;" + "Database=MinionsDB;" + "Integrated Security = true;");
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    names.Add((string)reader["Name"]);
+                }
+
+                reader.Close();
+
+                int counter = 1;
+
+                for (int i = 0; i < names.Count / 2; i++)
+                {
+                    Console.WriteLine(names[i]);
+                    Console.WriteLine(names[names.Count - counter]);
+                    counter++;
+                }
+            }
+        }
+    }
+}
