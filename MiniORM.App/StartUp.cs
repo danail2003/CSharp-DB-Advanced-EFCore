b87986dea711cd8760d2ba4e323d8b1e285a7858
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-03 11:38:46 +0200

    Add files via upload

---
+++ MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using Data;
+    using System.Linq;
+    using Data.Entities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string connectionString = "Server=DESKTOP-HUSD35E\\SQLEXPRESS;" + "Database=MiniORM;" + "Integrated Security=true;";
+
+            SoftUniDbContext context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
